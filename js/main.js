c265df5217fdd65a71ab59def15a216b6f3fa2e3
Volodymyr Udsianskiy <vv7827242@gmail.com>
2024-11-07 15:20:54 +0200

    ch

---
+++ js/main.js
@@ -0,0 +1,18 @@
+let result = 5 + 5 + '5';
+console.log(typeof (result));
+
+let email = 'vv7827242@gmail.com';
+console.log(email.includes("@"));
+console.log(email.length);
+
+let my = "My ";
+let name = "name ";
+let is = "is ";
+
+let fullName = my + name + is + "Victor";
+console.log(fullName);
+
+let userName = "Сергій";
+let payment = 3400;
+
+const full = alert(`Дякуємо, ${userName}! До сплати ${payment} гривень.`);
